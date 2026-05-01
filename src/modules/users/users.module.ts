eaf443e04854f9bba4fd78df6655e938dbf95167
Bassel Raafat <basselr11@gmail.com>
2026-05-02 00:25:06 +0300

    fix

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
-import { SupabaseService } from 'src/common/services/SupabaseService ';
+import { SupabaseService } from '../../common/services/SupabaseService ';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
