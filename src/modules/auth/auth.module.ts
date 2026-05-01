238762a08fafcf3932c15640abed5cdc66eedd04
Bassel Raafat <basselr11@gmail.com>
2026-05-02 00:11:40 +0300

    fix

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { SupabaseService } from 'src/common/services/SupabaseService ';
 
 @Module({
   imports: [UsersModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, SupabaseService],
   exports: [AuthService],
 })
 export class AuthModule {}
