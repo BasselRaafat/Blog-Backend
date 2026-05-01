ee3576d6f9d9592e12153849239c60fa87d5bf89
Bassel Raafat <basselr11@gmail.com>
2026-05-02 00:48:36 +0300

    chnaged the origin

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://blog-ghv4-1x28em9tc-basselraafats-projects.vercel.app',
+    origin: 'https://blog-ghv4.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // only if you're using cookies/auth headers
   });
